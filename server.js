0421a378d58fc9bc75cad3251b908a696647ac7d
Pritam Paul <pritampaulsdev@gmail.com>
2026-04-03 12:04:52 +0530

    database connection done using Sequelize and model folder is created (no other code is fixed as per sequelize)

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 const db = require("./utils/db_connection");
 const studentRoute = require("./routes/studentRoutes");
+//models
+const studentModel = require("./models/students");
 
 app.use(express.json());
 app.use("/students", studentRoute);
 
-app.listen(3000, (err) => {
-    console.log("Server is running in port no 3000");
-})
+db.sync()
+  .then(() => {
+    app.listen(3000, (err) => {
+      console.log("Server is running in port no 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- utils/db_connection.js
+++ utils/db_connection.js
@@ -1,31 +1,51 @@
-const mySql = require("mysql2");
-const connection = mySql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "PRIyaNPRIya",
-  database: "studentmanager",
-});
-connection.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Database connected");
-  }
-  const createQuery = `CREATE TABLE IF NOT EXISTS Students(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(20),
-        email VARCHAR(255) UNIQUE,
-        age INT
-    );`;
-  connection.execute(createQuery, (err, result) => {
-    if (err) {
-      console.log(err);
-      connection.end;
-      return;
-    } else {
-      console.log("Student database created");
-    }
-  });
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize('studentManager', 'root', 'PRIyaNPRIya', {
+    host: 'localhost',
+    dialect: "mysql"
 });
 
-module.exports = connection;
+(async () => {
+    try {
+        sequelize.authenticate();
+        console.log("Database is connected using Sequelize");
+    } catch(err) {
+        console.log(err);
+    }
+})();
+
+module.exports = sequelize;
+
+
+
+// const mySql = require("mysql2");
+// const connection = mySql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "PRIyaNPRIya",
+//   database: "studentmanager",
+// });
+// connection.connect((err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Database connected");
+//   }
+//   const createQuery = `CREATE TABLE IF NOT EXISTS Students(
+//         id INT AUTO_INCREMENT PRIMARY KEY,
+//         name VARCHAR(20),
+//         email VARCHAR(255) UNIQUE,
+//         age INT
+//     );`;
+//   connection.execute(createQuery, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       connection.end;
+//       return;
+//     } else {
+//       console.log("Student database created");
+//     }
+//   });
+// });
+
+// module.exports = connection;
